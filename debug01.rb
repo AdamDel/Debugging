1d2b236f19ff7adbdb27fadde862681cf9742634
AdamDel <adam.devbootcamp@gmail.com>
2016-01-08 18:47:07 +0000

    Initial Commit

---
+++ debug01.rb
@@ -0,0 +1,7 @@
+
+list = {'yvr' => 'Vancouver', 'yba' => 'Banff', 'yyz' => 'Toronto', 'yxx' => 'Abbotsford', 'ybw' => 'Calgary'}
+
+# Why is it returning nil instead of first element of the list above
+p list.first
+
+# p list[0] is for arrays but this is a hash, we have to use the syntax for a hash
